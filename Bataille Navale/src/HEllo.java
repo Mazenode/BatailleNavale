740622ef8929e8007cc37576c71e5011efbc4fea
Rysek <36196503+rezatera854@users.noreply.github.com>
2020-01-14 19:58:10 +0100

    Update HEllo.java

--- Bataille Navale/src/HEllo.java
+++ Bataille Navale/src/HEllo.java
@@ -1,9 +1,19 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
 
 public class Hello {
 
     
     public static void main(String[] args)  {
-        System.out.println("test");
+        int i,j=0;
+        int grille [] [] = new int [10] [10];
+        for(i=0;i < grille[0].length;i++){
+            System.out.println(grille[i][j]);
+            for(j=0;j<grille[1].length;j++){
+                    
+            }
+        }
+        
+
     }
     
 }
