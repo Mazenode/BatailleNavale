ee52526539c9f9fcbe0af78cc5f420fbab5cdc48
Quentin Chaignaud <quentinchaignaud@protonmail.com>
2020-01-18 19:03:56 +0100

    test

--- Bataille Navale/src/Model/DemoModel.java
+++ Bataille Navale/src/Model/DemoModel.java
@@ -1,5 +1,20 @@
 package Model;
 
-public class DemoModel {
+import javax.swing.ImageIcon;
 
+public class DemoModel {
+	
+	/*Idem que pour Menu Principal, on commence à initialiser à partir du 2ème texte.*/
+	private ImageIcon blank = new ImageIcon();
+	private ImageIcon imgText2 = new ImageIcon(this.getClass().getResource("/text2.png"));
+	
+	public ImageIcon getImage(int i) {
+		switch(i) {
+			case 1:
+				return imgText2;
+				
+			
+		}
+		return blank;
+	}
 }
--- Bataille Navale/src/View/DemoView.java
+++ Bataille Navale/src/View/DemoView.java
@@ -1,18 +1,43 @@
 package View;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class DemoView extends JFrame {
 	private int x = 1920;
 	private int y = 1080;
+	private ImageIcon imgBackground = new ImageIcon(this.getClass().getResource("/demo_background1.png"));
+	private ImageIcon imgText = new ImageIcon(this.getClass().getResource("/text1.png"));
+	private JLabel background, text;
 	
 	public DemoView(){
-		JPanel menuPanel = new JPanel();
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(x, y);
 		this.setExtendedState(JFrame.MAXIMIZED_BOTH); 
 		this.setUndecorated(true);
+		this.setVisible(true);
+		text = new JLabel(imgText);
+		text.setBounds(0,350,x,y);
+		add(text);
 		
+		background = new JLabel(imgBackground, JLabel.CENTER);
+		background.setBounds(0,0,x,y);
+		add(background);
+		
+		//menu.setContentPane(this.getPanel());
+        this.revalidate();
+	
+
 	}
+	
+	public final JLabel getBackgroundLabel(){
+        return background;
+    }
+	
+	public final JLabel getTextLabel(){
+        return text;
+    }
+	
 }
